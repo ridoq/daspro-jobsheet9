4ab015e22f9ea4476cd9d2612c141e8cdb6414b6
ridoq <ridoq444@gmail.com>
2025-11-13 06:57:09 +0700

    Percobaan 2 - Soal 3

--- ArrayNilai24.java
+++ ArrayNilai24.java
@@ -7,8 +7,10 @@ public class ArrayNilai24 {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for(int i = 0; i < nilaiAkhir.length; i++){
+            if (nilaiAkhir[i] > 70){
+                System.out.println("Mahasiswa ke-" + i + " lulus!");
+            }
         }
         sc.close();
     }
