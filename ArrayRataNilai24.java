abd998188af92e1abc94d692ab3a77bcc2f247db
ridoq <ridoq444@gmail.com>
2025-11-13 07:59:38 +0700

    Percobaan 3 - Soal 2

--- ArrayRataNilai24.java
+++ ArrayRataNilai24.java
@@ -2,28 +2,36 @@ import java.util.Scanner;
 public class ArrayRataNilai24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        //modifikasi no1
-        int jumlahMhsLulus = 0;
-        //
-        double total = 0, rata2;
+        int jumlahMhsLulus = 0, jumlahMhsTdkLulus = 0;
+        double totalMhsLulus = 0,totalMhsTdkLulus = 0, rata2MhsLulus, rata2MhsTdkLulus;
+        //modifikasi no1:
+        // int jumlahMhsLulus = 0;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahMhs];
         for(int i = 0; i < nilaiMhs.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
         }
         for(int i = 0; i < nilaiMhs.length; i++){
-            //modifikasi no1
+            //modifikasi no1:
+            // if (nilaiMhs[i] > 70) {
+            //     jumlahMhsLulus += 1;
+            // }
             if (nilaiMhs[i] > 70) {
-                jumlahMhsLulus += 1;
+                totalMhsLulus += nilaiMhs[i];
+                jumlahMhsLulus++;
+            }else{
+                totalMhsTdkLulus += nilaiMhs[i];
+                jumlahMhsTdkLulus++;
             }
-            //
-            total += nilaiMhs[i];
         }
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
-        //modifikasi no1
-        System.out.println("Jumlah mahasiswa yang lulus (nilai lebih dari 70) = " + jumlahMhsLulus);
-        //
+        rata2MhsLulus = totalMhsLulus / jumlahMhsLulus;
+        rata2MhsTdkLulus = totalMhsTdkLulus / jumlahMhsTdkLulus;
+        System.out.println("Rata-rata nilai lulus = " + rata2MhsLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2MhsTdkLulus);
+        //modifikasi no1:
+        // System.out.println("Jumlah mahasiswa yang lulus (nilai lebih dari 70) = " + jumlahMhsLulus);
         sc.close();
     }
 }
--- README.md
+++ README.md
@@ -160,4 +160,33 @@
     //
     ```
 
-2. 
+2. Berikut hasil modifikasi nomor 2:
+
+    ```java
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jumlahMhsLulus = 0, jumlahMhsTdkLulus = 0;
+        double totalMhsLulus = 0,totalMhsTdkLulus = 0, rata2MhsLulus, rata2MhsTdkLulus;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jumlahMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahMhs];
+        for(int i = 0; i < nilaiMhs.length; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = sc.nextInt();
+        }
+        for(int i = 0; i < nilaiMhs.length; i++){
+            if (nilaiMhs[i] > 70) {
+                totalMhsLulus += nilaiMhs[i];
+                jumlahMhsLulus++;
+            }else{
+                totalMhsTdkLulus += nilaiMhs[i];
+                jumlahMhsTdkLulus++;
+            }
+        }
+        rata2MhsLulus = totalMhsLulus / jumlahMhsLulus;
+        rata2MhsTdkLulus = totalMhsTdkLulus / jumlahMhsTdkLulus;
+        System.out.println("Rata-rata nilai lulus = " + rata2MhsLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2MhsTdkLulus);
+        sc.close();
+    }
+    ```
